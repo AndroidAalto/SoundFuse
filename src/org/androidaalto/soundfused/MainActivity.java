ca25f2c33669f80dcc56e01ab015f1b99ea509af
Gerard Lledo <gerard.lledo@gmail.com>
2011-03-26 18:32:19 +0200

    Get 4 more realistic samples
    Taken from http://pattern86.com/apps/sequencer-in-html5/

--- src/org/androidaalto/soundfused/MainActivity.java
+++ src/org/androidaalto/soundfused/MainActivity.java
@@ -4,6 +4,7 @@ package org.androidaalto.soundfused;
 import android.app.Activity;
 import android.media.SoundPool;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.util.Log;
 
 public class MainActivity extends Activity {
@@ -18,9 +19,20 @@ public class MainActivity extends Activity {
     private void initSounds() {
         // 4 streams
         // stream type 3 is music
-        SoundPool soundPool = new SoundPool(4, 3, 100);
-        int explosionSound = soundPool.load(this.getBaseContext(), R.raw.example, 1);
-        while (soundPool.play(explosionSound, 100, 100, 1, 0, 1) == 0)
-            Log.e("SoundPool", "Unable to play sound");
+        SoundPool soundPool = new SoundPool(8, 3, 100);
+        int N_SOUNDS = 4;
+        int sounds[] = new int[N_SOUNDS];
+        sounds[0] = soundPool.load(this.getBaseContext(), R.raw.bass, 1);
+        sounds[1] = soundPool.load(this.getBaseContext(), R.raw.hhc, 1);
+        sounds[2] = soundPool.load(this.getBaseContext(), R.raw.hho, 1);
+        sounds[3] = soundPool.load(this.getBaseContext(), R.raw.snare, 1);
+        
+        int i = 0;
+        while (true) {
+            long start = System.currentTimeMillis();
+            soundPool.play(sounds[i], 100, 100, 1, 0, 1);
+            i = (i+1) % N_SOUNDS;
+            SystemClock.sleep(start + 1000 - System.currentTimeMillis());
+        }
     }
 }
