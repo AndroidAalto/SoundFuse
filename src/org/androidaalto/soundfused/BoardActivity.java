2583c36ae743f1b6cb69e3b261c1006c49614adf
jush <oni.pool@gmail.com>
2011-03-26 21:29:49 +0200

    UI connected to sequencer library.

--- src/org/androidaalto/soundfused/BoardActivity.java
+++ src/org/androidaalto/soundfused/BoardActivity.java
@@ -1,6 +1,8 @@
 
 package org.androidaalto.soundfused;
 
+import org.androidaalto.soundfused.sequencer.Sequencer;
+
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Color;
@@ -14,27 +16,36 @@ import android.widget.LinearLayout;
 import android.widget.ToggleButton;
 
 public class BoardActivity extends Activity {
-    private static final int TIMELINE = 8;
+    public static final int TOTAL_BEATS = 8;
 
-    private static final int SAMPLERS = 4;
+    public static final int TOTAL_SAMPLES = 4;
 
     LinearLayout mainLayout;
 
-    ToggleButton samplersButtons[][] = new ToggleButton[SAMPLERS][TIMELINE];
+    Sequencer sequencer;
 
-    LinearLayout boardLayouts[] = new LinearLayout[SAMPLERS];
+    ToggleButton samplersButtons[][] = new ToggleButton[TOTAL_SAMPLES][TOTAL_BEATS];
+
+    LinearLayout boardLayouts[] = new LinearLayout[TOTAL_SAMPLES];
 
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        sequencer = new Sequencer(this, TOTAL_SAMPLES, TOTAL_BEATS);
+        sequencer.setSample(0, R.raw.bass);
+        sequencer.setSample(1, R.raw.hhc);
+        sequencer.setSample(2, R.raw.hho);
+        sequencer.setSample(3, R.raw.snare);
+        sequencer.play();
+
+        // Use the whole device screen.
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         prepareBoard();
-
     }
 
     private void prepareBoard() {
@@ -48,12 +59,12 @@ public class BoardActivity extends Activity {
         mainLayout.setOrientation(LinearLayout.VERTICAL);
         mainLayout.setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT,
                 LayoutParams.FILL_PARENT));
-        for (int samplerPos = 0; samplerPos < SAMPLERS; samplerPos++) {
-            boardLayouts[samplerPos] = new LinearLayout(this);
-            boardLayouts[samplerPos].setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT,
+        for (int samplePos = 0; samplePos < TOTAL_SAMPLES; samplePos++) {
+            boardLayouts[samplePos] = new LinearLayout(this);
+            boardLayouts[samplePos].setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT,
                     LayoutParams.WRAP_CONTENT));
-            boardLayouts[samplerPos].setBackgroundColor(Color.rgb(255, 0, 0));
-            mainLayout.addView(boardLayouts[samplerPos]);
+            boardLayouts[samplePos].setBackgroundColor(Color.rgb(255, 0, 0));
+            mainLayout.addView(boardLayouts[samplePos]);
         }
 
     }
@@ -61,26 +72,25 @@ public class BoardActivity extends Activity {
     private void createBoardButtons() {
         Display display = ((WindowManager) getSystemService(Context.WINDOW_SERVICE))
                 .getDefaultDisplay();
-        int buttonWidth = display.getWidth() / TIMELINE;
-        int buttonHeight = display.getHeight() / SAMPLERS;
-        
-        SamplerToggleListener samplerListener = new SamplerToggleListener();
-        
-        for (int samplerPos = 0; samplerPos < SAMPLERS; samplerPos++) {
+        int buttonWidth = display.getWidth() / TOTAL_BEATS;
+        int buttonHeight = display.getHeight() / TOTAL_SAMPLES;
+
+        SamplerToggleListener samplerListener = new SamplerToggleListener(sequencer, this,
+                TOTAL_SAMPLES, TOTAL_BEATS);
+
+        for (int samplePos = 0; samplePos < TOTAL_SAMPLES; samplePos++) {
             Log.d("Board", "Button width: " + buttonWidth);
-            for (int timelinePos = 0; timelinePos < TIMELINE; timelinePos++) {
-                samplersButtons[samplerPos][timelinePos] = new ToggleButton(this);
-                samplersButtons[samplerPos][timelinePos].setTextOff("(" + samplerPos + ","
-                        + timelinePos + ")");
-                samplersButtons[samplerPos][timelinePos].setTextOn("(" + samplerPos + ","
-                        + timelinePos + ")");
-                samplersButtons[samplerPos][timelinePos].setText("(" + samplerPos + ","
-                        + timelinePos + ")");
-                samplersButtons[samplerPos][timelinePos].setWidth(buttonWidth);
-                samplersButtons[samplerPos][timelinePos].setHeight(buttonHeight);
-                samplersButtons[samplerPos][timelinePos].setId(TIMELINE * samplerPos + timelinePos);
-                samplersButtons[samplerPos][timelinePos].setOnClickListener(samplerListener);
-                boardLayouts[samplerPos].addView(samplersButtons[samplerPos][timelinePos]);
+            for (int beatPos = 0; beatPos < TOTAL_BEATS; beatPos++) {
+                samplersButtons[samplePos][beatPos] = new ToggleButton(this);
+                samplersButtons[samplePos][beatPos].setTextOff("");
+                samplersButtons[samplePos][beatPos].setTextOn("");
+                samplersButtons[samplePos][beatPos].setText("");
+                samplersButtons[samplePos][beatPos].setWidth(buttonWidth);
+                samplersButtons[samplePos][beatPos].setHeight(buttonHeight);
+                samplersButtons[samplePos][beatPos].setId(TOTAL_BEATS * samplePos
+                        + beatPos);
+                samplersButtons[samplePos][beatPos].setOnClickListener(samplerListener);
+                boardLayouts[samplePos].addView(samplersButtons[samplePos][beatPos]);
             }
         }
     }
--- src/org/androidaalto/soundfused/SamplerToggleListener.java
+++ src/org/androidaalto/soundfused/SamplerToggleListener.java
@@ -1,14 +1,36 @@
+
 package org.androidaalto.soundfused;
 
+import org.androidaalto.soundfused.sequencer.Sequencer;
+
+import android.content.Context;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.ToggleButton;
 
-public class SamplerToggleListener implements OnClickListener{
+public class SamplerToggleListener implements OnClickListener {
+
+    Sequencer sequencer;
+
+    public SamplerToggleListener(Sequencer sequencer, Context ctx, int samples, int beats) {
+        this.sequencer = sequencer;
+    }
 
     @Override
     public void onClick(View v) {
-        Log.d("SampleToggleListener" , "Button id: " + v.getId());
-    }
+        int buttonId = v.getId();
 
+        int samplerId = buttonId / BoardActivity.TOTAL_BEATS;
+        int beatId = buttonId % BoardActivity.TOTAL_BEATS;
+        if (!(v instanceof ToggleButton)) {
+            Log.e("SampleToggleListener", "Invalid View type: " + v.getClass());
+            return;
+        }
+        ToggleButton currentButton = (ToggleButton) v;
+        if (currentButton.isChecked())
+            sequencer.enableCell(samplerId, beatId);
+        else
+            sequencer.disableCell(samplerId, beatId);
+    }
 }
